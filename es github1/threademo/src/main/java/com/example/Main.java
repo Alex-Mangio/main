f33a2d0cad18173da64005d4d97abda432154965
Alex-Mangio <163597290+Alex-Mangio@users.noreply.github.com>
2024-03-21 21:31:24 +0100

    Add files via upload

---
+++ es github1/threademo/src/main/java/com/example/Main.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import java.awt.Dimension;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(new Dimension(400,400));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
